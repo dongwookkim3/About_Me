968be4c18ad74e3f6682be3662fff3b71b283087
dongwookkim3 <dongwook.3.kim@gmail.com>
2024-04-14 02:06:45 +0900

    REFACTOR : Svelte적용

---
+++ src/App.svelte
@@ -0,0 +1,70 @@
+<script>
+
+</script>
+
+<main>
+	<h1>I'm Dongwook an</h1>
+	<h1>Embedded Developer</h1>
+	<div>
+		<a href="https://www.instagram.com/d0ngwook7/" target="_blank">
+			<img src="images/instagram.svg" alt="Instagram"></a>
+		<a href="https://swbsnewby7.tistory.com/" target="_blank">
+			<img src="images/tistory.svg" alt="Tistory"></a>
+		<a href="https://github.com/dongwookkim3" target="_blank">
+			<img src="images/github.svg" alt="GitHub"></a>
+		<a href="https://www.notion.so/4bcee3d2019442308d332e9be3b0891c" target="_blank">
+			<img src="images/notion.svg" alt="Notion"></a>
+		<a href="https://solved.ac/profile/dongwook7" target="_blank">
+			<img src="images/solvedac.png" alt="Solved.ac"></a>
+		<a href="https://career.programmers.co.kr/pr/dongwook3kim" target="_blank">
+			<img src="images/programmers.png" alt="Programmers"></a>
+	</div>
+</main>
+
+<style>
+	:global(body) {
+		background-image: url('/images/background.jpg');
+		background-size: cover;
+		background-repeat: no-repeat;
+		background-attachment: fixed;
+		background-position: center;
+		position: fixed;
+		top: 0;
+		left: 0;
+		width: 100%;
+		height: 100%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+
+	:global(h1) {
+		margin: 0;
+		font-size: 96px;
+		color: #fff;
+		font-family: 'CookieRun Bold', sans-serif;
+	}
+
+	:global(img) {
+		width: 130px;
+		border-radius: 15%;
+	}
+
+	:global(a) {
+		margin-right: 7px;
+	}
+
+	:global(div) {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		height: 60%;
+		padding: 20px;
+	}
+
+	@font-face {
+		font-family: 'CookieRun Bold';
+		src: url('/fonts/CookieRun Bold.otf') format('opentype');
+	}
+</style>
